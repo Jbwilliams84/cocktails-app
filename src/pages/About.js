c7f3be4394a1edb1a4e6f72036a545f733e88da7
Jbwilliams84 <53539672+Jbwilliams84@users.noreply.github.com>
2021-03-16 08:44:34 -0500

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<section className='section about-section'>
+			<h1 className='section-title'>about us</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo laudantium
+				officiis delectus cum, rerum repellendus nihil esse sunt voluptas
+				corporis a magnam impedit perferendis sed magni repellat minus mollitia
+				consequuntur?
+			</p>
+		</section>
+	);
+};
+
+export default About;
